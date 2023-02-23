02e56a11f0b9a504899438db85001b7dcaa5f180
abdevelo <talove0531@gmail.com>
2023-02-23 17:30:24 +0900

    to merge P73

--- day02/src/variable/P73.java
+++ day02/src/variable/P73.java
@@ -5,10 +5,12 @@ import java.util.Scanner;
 public class P73 {
 
 	public static void main(String[] args) {
+		int a = 10 ;
 		Scanner sc = new Scanner(System.in);
 		System.out.println("명령어를 입력 하시요(q,i,d,s)");
 		String cmd = sc.next(); // cmd에 다음 화면에 입력한 값을 넣으시오.
 		System.out.printf("%s를 입력 했습니다.", cmd);
-		sc.close(); // application이 next를 통해 입력값을 저장하는 pipe를 열어둔 후에는 꼭 이를 닫아주어야 함.
+		sc.close(); 
+		// application이 next를 통해 입력값을 저장하는 pipe를 열어둔 후에는 꼭 이를 닫아주어야 함.
 	}
 }
