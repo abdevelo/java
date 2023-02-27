04ebe7f9f52d55fe50b792c95ff2d06cb2a62a3d
abdevelo <talove0531@gmail.com>
2023-02-27 10:13:39 +0900

    2023 02 27

--- day03/src/whilecontrol/P99.java
+++ day03/src/whilecontrol/P99.java
@@ -4,19 +4,16 @@ public class P99 {
 
 	public static void main(String[] args) {
 		
-		int bb = 200;
-		int a = 10;
-		
-		int bb = 100;
-		int b = 20;
-		int c = 20;
-		int cc = 100;
-	
+		myfor:
 		for (int i = 2; i < 10; i++) {
 			System.out.printf("%d 단 \n", i);
 			for (int j = 1; j < 10; j++) {
-				System.out.printf("%d * %d = %d \n", i, j, (i * j));
+				System.out.printf("%d * %d = %d \t", i, j, (i * j));
+				if ((i*j) == 21) {
+					break myfor;
+				}
 			}
+			System.out.println("");
 		}
 	}
 }
