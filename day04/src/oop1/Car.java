b388c75edc1aa997d2d0025d5afddbf95d44e229
abdevelo <talove0531@gmail.com>
2023-02-28 13:04:08 +0900

    2023 02 28 img

---
+++ day04/src/oop1/Car.java
@@ -0,0 +1,29 @@
+package oop1;
+
+public class Car {
+	// attribute 속성
+	public String name ;
+	public String color; 
+	public int size; 
+	
+	// constructor 생성자.
+	public Car() {
+		this.name = "k1"; 
+		this.color = "red";
+		this.size = 1000;
+	}
+	public Car(String name, String color, int size) {
+		this.name = name; 
+		this.color = color;
+		this.size = size;
+	}
+		
+	// operation 행위
+	public void go() {
+		System.out.printf("%s, %s Go Car ...", this.name, this.color);
+	}
+	public void stop() {
+		System.out.printf("%s, %s Stop Car ...", this.name, this.color);
+	}
+
+}
